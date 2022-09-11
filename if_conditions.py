97fdc88674ba77fb9233cbc6b1007506de8d456b
Senaraj Chintagumpala <senaraj@Senarajs-Macbook-Pro.local>
2022-09-11 15:55:14 +0530

    adding a new file

---
+++ if_conditions.py
@@ -0,0 +1,19 @@
+a = 200
+b = 23
+c = 500
+#if b > a:
+ #   print("b is greater than a")
+
+#elif a == b:
+    #print('a and b are of same value')
+
+#else:
+ #   print("a is of greater value than b")
+
+#print("A") if a > b else print("B")
+
+#if a > b or c < a:
+ #   print("atleast one of the conditions are true")
+
+if b > a:
+    pass
